a24b96e81f66890aacc3e972605bd1a68b5bf5f7
Hamada Assi <1181121@student.birzeit.edu>
2025-11-08 20:07:18 +0200

    update configuration

--- starter/gulpfile.js
+++ starter/gulpfile.js
@@ -2,11 +2,11 @@ import gulp from "gulp";
 import shell from "gulp-shell";
 import mocha from "gulp-mocha";
 
-gulp.task("parcel", shell.task("npx parcel index.html"));
+gulp.task("parcel", shell.task("npx parcel build index.html --dist-dir dist"));
 
 gulp.task("test", () => {
   return gulp.src("test/**/*.js", { read: false })
     .pipe(mocha({ reporter: "spec" }));
 });
 
-gulp.task("default", gulp.series("parcel"));
+gulp.task("default", gulp.series("parcel", "test"));
